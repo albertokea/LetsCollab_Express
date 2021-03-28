56431760c7363da7bbc7ae4751e7e638d0c04349
albertokea <atorrescota5@gmail.com>
2021-03-28 20:54:59 +0200

    Añadida get a users con paginación

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,66 +2,18 @@ const router = require('express').Router();
 const multer = require('multer');
 const uploadProfileImage = multer({ dest: 'public/images/profile_pictures' });
 const uploadHeader = multer({ dest: 'public/images/header_pictures' });
-const { getAll, getById, getByEmail, getByUser, updateProfile, deleteById, updateHeader } = require('../../models/user');
+const { getAll, getCountUsers, getById, getByEmail, getByUser, updateProfile, deleteById, updateHeader } = require('../../models/user');
 const fs = require('fs')
 
-//Edit
-router.put('/update', uploadProfileImage.single('profile_picture'), async (req, res, next) => {
-    if (req.file) {
-        const extension = '.' + req.file.mimetype.split('/')[1];
-        const newName = req.file.filename + extension;
-        const newPath = req.file.path + extension;
-        fs.renameSync(req.file.path, newPath);
-        req.body.profile_picture = newName
-
-        try {
-            const result = await updateProfile(req.body);
-            res.json(result)
-        }
-        catch (error) {
-            res.status(422).json({ error: error.message });
-        }
-    } else {
-
-        try {
-            const result = await updateProfile(req.body);
-            res.json(result)
-        }
-        catch (error) {
-            res.status(422).json({ error: error.message });
-        }
-    }
-
-
-});
-router.put('/updateHeader', uploadHeader.single('header_picture'), async (req, res, next) => {
-
-    const extension = '.' + req.file.mimetype.split('/')[1];
-    const newName = req.file.filename + extension;
-    const newPath = req.file.path + extension;
-    fs.renameSync(req.file.path, newPath);
-    req.body.header_picture = newName
-    req.body.iduser = req.userId
-
-
-    try {
-        console.log(req.body);
-        const result = await updateHeader(req.body);
-        res.json(result)
-    }
-    catch (error) {
-        res.status(422).json({ error: error.message });
-
-    }
-
-
-});
-
 //GET all users
-router.get('/', async (req, res) => {
+router.get('/offset/:offset', async (req, res) => {
     try {
-        const result = await getAll();
-        res.json(result)
+        const data = {};
+        const result = await getAll(parseInt(req.params.offset));
+        const count = await getCountUsers();
+        data.result = result;
+        data.info = { count, pages: Math.ceil(count / 10) }
+        res.json(data);
     }
     catch (error) {
         res.status(422).json({ error: error.message });
@@ -99,8 +51,67 @@ router.get('/user/:user', async (req, res) => {
     catch (error) {
         res.status(422).json({ error: error.message });
     }
-})
+});
 
+//GET by subtitle
+router.get('/subtitle/:subtitle', async (req, res) => {
+    try {
+        const result = await getByUser(req.params.user);
+        res.json(result)
+    }
+    catch (error) {
+        res.status(422).json({ error: error.message });
+    }
+});
+
+//Edit (profile_picture included)
+router.put('/update', uploadProfileImage.single('profile_picture'), async (req, res, next) => {
+    if (req.file) {
+        const extension = '.' + req.file.mimetype.split('/')[1];
+        const newName = req.file.filename + extension;
+        const newPath = req.file.path + extension;
+        fs.renameSync(req.file.path, newPath);
+        req.body.profile_picture = newName
+
+        try {
+            const result = await updateProfile(req.body);
+            res.json(result)
+        }
+        catch (error) {
+            res.status(422).json({ error: error.message });
+        }
+    } else {
+
+        try {
+            const result = await updateProfile(req.body);
+            res.json(result)
+        }
+        catch (error) {
+            res.status(422).json({ error: error.message });
+        }
+    }
+});
+
+//Update header
+router.put('/updateHeader', uploadHeader.single('header_picture'), async (req, res, next) => {
+    const extension = '.' + req.file.mimetype.split('/')[1];
+    const newName = req.file.filename + extension;
+    const newPath = req.file.path + extension;
+    fs.renameSync(req.file.path, newPath);
+    req.body.header_picture = newName
+    req.body.iduser = req.userId
+
+    try {
+        console.log(req.body);
+        const result = await updateHeader(req.body);
+        res.json(result)
+    }
+    catch (error) {
+        res.status(422).json({ error: error.message });
+    }
+});
+
+//Delete
 router.delete('/delete/:iduser', async (req, res) => {
     try {
         const result = await deleteById(req.params.iduser);
